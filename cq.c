ae924910f7a0483a23b56bfb93150d42d3e2f1f1
Rohan Drape <rd@slavepianos.org>
2011-11-29 02:52:02 +0000

    cq_index,cq_access,cq_update

--- cq.c
+++ cq.c
@@ -1,4 +1,5 @@
 #include "cq.h"
+#include "signal-interpolate.h"
 
 /* cq = circular queue
    n = buffer length
@@ -6,6 +7,15 @@
    wi = write index
    ai = absolute index
    s = buffer */
+
+float cq_index(int n,float ri,int wi)
+{
+  float ai = ((float)wi - 1.0) - ri;
+  if(ai < 0.0) {
+    ai += (float)n;
+  }
+  return ai ;
+}
 
 int cq_index_i(int n,int ri,int wi)
 {
@@ -22,10 +32,24 @@ float cq_access_i(float *s,int n,int ri,int wi)
   return s[i];
 }
 
+float cq_access(float *s,int n,float ri,int wi)
+{
+  int i = cq_index(n,ri,wi);
+  return signal_interpolate(s,n,i);
+}
+
 void cq_increment_write_index(int n,int *wi)
 {
   *wi += 1;
   if(*wi >= n) {
     *wi = 0;
   }
+}
+
+float cq_update(float *s,int n,float i,int *wi)
+{
+  float j = s[*wi];
+  s[*wi] = i;
+  cq_increment_write_index(n,wi);
+  return j;
 }
--- cq.h
+++ cq.h
@@ -1,8 +1,11 @@
 #ifndef _COMMON_CQ_H
 #define _COMMON_CQ_H
 
+float cq_index(int n,float ri,int wi);
 int cq_index_i(int n,int ri,int wi);
 float cq_access_i(float *s,int n,int ri,int wi);
+float cq_access(float *s,int n,float ri,int wi);
 void cq_increment_write_index(int n,int *wi);
+float cq_update(float *s,int n,float i,int *wi);
 
 #endif
