fe79f8b4eca9b5ffed1f5e06620f392f6f7a15f2
Rohan Drape <rd@slavepianos.org>
2014-12-12 02:45:35 +0000

    ~signal-copy (non memcpy variant)

--- signal-copy.c
+++ signal-copy.c
@@ -8,3 +8,9 @@ void signal_copy_circular ( float *dst , const float *src , int n , int s )
   memcpy ( dst , src + s , ( n - s ) * sizeof(float) ) ;
   memcpy ( dst + ( n - s ) , src , s * sizeof(float) ) ;
 }
+
+/*
+  int d = n - s;
+  for(int i = 0; i < d; i++) { dst[i] = src[s + i]; }
+  for(int i = 0; i < s; i++) { dst[i + d] = src[i]; }
+*/
