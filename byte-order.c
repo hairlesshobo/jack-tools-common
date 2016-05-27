9cda807094381238ae61ce3ebb895a9ed8e3f3c7
Rohan Drape <rd@slavepianos.org>
2016-05-27 04:41:38 +0000

    -tab

--- byte-order.c
+++ byte-order.c
@@ -48,11 +48,11 @@ void byte_order_confirm(void)
   }
 }
 
-#define GENERATE_NC_TO_BUF(proc,size)		\
-  void proc##size##_to_buf(u8 *buf,u##size n)	\
-  {						\
-    n = proc##size(n);				\
-    memcpy(buf,&n,size/8);			\
+#define GENERATE_NC_TO_BUF(proc,size)           \
+  void proc##size##_to_buf(u8 *buf,u##size n)   \
+  {                                             \
+    n = proc##size(n);                          \
+    memcpy(buf,&n,size/8);                      \
   }
 
 GENERATE_NC_TO_BUF(hton,16)
@@ -62,12 +62,12 @@ GENERATE_NC_TO_BUF(ntoh,32)
 GENERATE_NC_TO_BUF(hton,64)
 GENERATE_NC_TO_BUF(ntoh,64)
 
-#define GENERATE_NC_FROM_BUF(proc,size)		\
-  u##size proc##size##_from_buf(const u8 *buf)		\
-  {						\
-    u##size n;					\
-    memcpy(&n,buf,size/8);			\
-    return proc##size(n);			\
+#define GENERATE_NC_FROM_BUF(proc,size)         \
+  u##size proc##size##_from_buf(const u8 *buf)  \
+  {                                             \
+    u##size n;                                  \
+    memcpy(&n,buf,size/8);                      \
+    return proc##size(n);                       \
   }
 
 GENERATE_NC_FROM_BUF(hton,16)
@@ -77,11 +77,11 @@ GENERATE_NC_FROM_BUF(ntoh,32)
 GENERATE_NC_FROM_BUF(hton,64)
 GENERATE_NC_FROM_BUF(ntoh,64)
 
-#define GENERATE_NC_BUF(proc,size)		\
-  void proc##size##_buf(u8 *dst,const u8 *src)	\
-  {						\
-    u##size n = proc##size##_from_buf(src);	\
-    memcpy(dst,&n,size/8);			\
+#define GENERATE_NC_BUF(proc,size)              \
+  void proc##size##_buf(u8 *dst,const u8 *src)  \
+  {                                             \
+    u##size n = proc##size##_from_buf(src);     \
+    memcpy(dst,&n,size/8);                      \
   }
 
 GENERATE_NC_BUF(hton,16)
@@ -91,12 +91,12 @@ GENERATE_NC_BUF(ntoh,32)
 GENERATE_NC_BUF(hton,64)
 GENERATE_NC_BUF(ntoh,64)
 
-#define GENERATE_TNC_TO_BUF(proc,tag,size)		\
-  void proc##_##tag##size##_to_buf(u8 *buf,tag##size a)	\
-  {							\
-    n##size##_t n;					\
-    n.tag = a;						\
-    proc##size##_to_buf(buf,n.u);			\
+#define GENERATE_TNC_TO_BUF(proc,tag,size)              \
+  void proc##_##tag##size##_to_buf(u8 *buf,tag##size a) \
+  {                                                     \
+    n##size##_t n;                                      \
+    n.tag = a;                                          \
+    proc##size##_to_buf(buf,n.u);                       \
   }
 
 GENERATE_TNC_TO_BUF(hton,u,16)
@@ -116,12 +116,12 @@ GENERATE_TNC_TO_BUF(ntoh,i,64)
 GENERATE_TNC_TO_BUF(hton,f,64)
 GENERATE_TNC_TO_BUF(ntoh,f,64)
 
-#define GENERATE_TNC_FROM_BUF(proc,tag,size)			\
-  tag##size proc##_##tag##size##_from_buf(const u8 *buf)	\
-  {								\
-    n##size##_t n;						\
-    n.u  = proc##size##_from_buf(buf);				\
-    return n.tag;						\
+#define GENERATE_TNC_FROM_BUF(proc,tag,size)                    \
+  tag##size proc##_##tag##size##_from_buf(const u8 *buf)        \
+  {                                                             \
+    n##size##_t n;                                              \
+    n.u  = proc##size##_from_buf(buf);                          \
+    return n.tag;                                               \
   }
 
 GENERATE_TNC_FROM_BUF(hton,u,16)
@@ -141,12 +141,12 @@ GENERATE_TNC_FROM_BUF(ntoh,i,64)
 GENERATE_TNC_FROM_BUF(hton,f,64)
 GENERATE_TNC_FROM_BUF(ntoh,f,64)
 
-#define GENERATE_TNC(proc,tag)			\
-  tag						\
-  proc##_##tag(tag a)				\
-  {						\
-    proc##_##tag##_to_buf((u8 *)&a,a);		\
-    return a;					\
+#define GENERATE_TNC(proc,tag)                  \
+  tag                                           \
+  proc##_##tag(tag a)                           \
+  {                                             \
+    proc##_##tag##_to_buf((u8 *)&a,a);          \
+    return a;                                   \
   }
 
 GENERATE_TNC(hton,u16)
