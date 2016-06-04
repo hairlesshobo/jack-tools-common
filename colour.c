306ca8e7fb5c25afaf7a0beff3d485a2709dddd9
Rohan Drape <rd@slavepianos.org>
2016-06-04 04:51:23 +0000

    ~~colour

--- colour.c
+++ colour.c
@@ -1,6 +1,7 @@
 #include "colour.h"
 #include "float.h"
 #include "int.h"
+#include "memory.h"
 
 void rgb_mul(u8 *c,f32 n)
 {
@@ -32,3 +33,16 @@ u8 ega_rgb_colours[64][3] =
   {255,85,0},{255,85,170},{255,255,0},{255,255,170},
   {85,85,85},{85,85,255},{85,255,85},{85,255,255},
   {255,85,85},{255,85,255},{255,255,85},{255,255,255}};
+
+void ega_rgb_lookup_i32(i32 ix,u8 *c)
+{
+  xmemcpy(c,ega_rgb_colours[ix],3);
+}
+
+/* n is in (0.0,1.0) */
+void ega_rgb_lookup_f32(f32 n,u8 *c)
+{
+  i32 ix = i32_min(n * 64,64);
+  xmemcpy(c,ega_rgb_colours[ix],3);
+}
+
