90bbef8dcaee6ff102adba90d5e5493c7c1f6d33
rd <rd@slavepianos.org>
2019-02-23 00:26:18 +0000

    ~midi,+vprintf

--- midi.c
+++ midi.c
@@ -1,5 +1,26 @@
 #include "midi.h"
 
-u8 status_ty(u8 st) {return (st >> 4);}
-u8 status_ch(u8 st) {return (st & 0x0F);}
-bool is_note_data(u8 ty) {return (ty == 0x8 || ty == 0x9);}
+u8 status_ty(u8 st) {
+    return (st >> 4);
+}
+
+u8 status_ch(u8 st) {
+    return (st & 0x0F);
+}
+
+bool is_note_data_ty(u8 ty) {
+    return (ty == 0x8 || ty == 0x9);
+}
+
+bool is_note_data(u8 st) {
+    return is_note_data_ty(status_ty(st));
+}
+
+bool is_channel_voice_message(u8 st) {
+    u8 n = status_ty(st);
+    return (n >= 0x8 && n < 0xF);
+}
+
+bool is_sysex_message(u8 st) {
+    return st == 0xF0;
+}
--- print.h
+++ print.h
@@ -3,8 +3,13 @@
 
 #include <stdio.h>
 
+/* error-printer */
 #define eprintf(...) fprintf(stderr,__VA_ARGS__)
 
+/* verbose-printer */
+#define vprintf(_verbose,...) if(_verbose) {fprintf(stderr,__VA_ARGS__);}
+
+/* debug-printer */
 #ifdef DEBUG
   #define dprintf(...) fprintf(stderr,__VA_ARGS__)
 #else
