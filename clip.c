94a61af240e805b485fc16606a20f6ef4047bafd
rd <rd@slavepianos.org>
2019-05-04 00:40:01 +0000

    +clip_typ

--- clip.c
+++ clip.c
@@ -3,31 +3,19 @@
 #include "float.h"
 #include "vector.h"
 
-i32 clipi32(i32 n, i32 l, i32 r)
+i32 clip_i32(i32 n, i32 l, i32 r)
 {
-  if(n < l) {
-      return l;
-  } else if(n > r) {
-      return r;
-  } else {
-      return n;
-  }
+    return clip_typ(n,l,r);
 }
 
-f32 clipf32(f32 n, f32 l, f32 r)
+f32 clip_f32(f32 n, f32 l, f32 r)
 {
-  if(n < l) {
-      return l;
-  } else if(n > r) {
-      return r;
-  } else {
-      return n;
-  }
+    return clip_typ(n,l,r);
 }
 
-v32 clipv32(v32 v, f32 l, f32 r)
+v32 clip_v32(v32 v, f32 l, f32 r)
 {
-  return make_v32(clipf32(v.x, l, r),
-		  clipf32(v.y, l, r),
-		  clipf32(v.z, l, r));
+    return make_v32(clip_f32(v.x, l, r),
+                    clip_f32(v.y, l, r),
+                    clip_f32(v.z, l, r));
 }
--- clip.h
+++ clip.h
@@ -5,8 +5,10 @@
 #include "float.h"
 #include "vector.h"
 
-i32 clipi32(i32 n, i32 l, i32 r);
-f32 clipf32(f32 n, f32 l, f32 r);
-v32 clipv32(v32 v, f32 l, f32 r);
+#define clip_typ(n,l,r) (n < l ? l : (n > r ? r : n))
+
+i32 clip_i32(i32 n, i32 l, i32 r);
+f32 clip_f32(f32 n, f32 l, f32 r);
+v32 clip_v32(v32 v, f32 l, f32 r);
 
 #endif
