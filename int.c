63633d76d5d4340e0771d69dd357ffb79b1bf650
rd <rd@slavepianos.org>
2020-09-23 04:21:26 +0000

    +u64_max

--- int.c
+++ int.c
@@ -1,3 +1,7 @@
 #include "int.h"
 
 i32 i32_min(i32 p,i32 q) {if(p < q) {return p;} return q;}
+i32 i32_max(i32 p,i32 q) {if(p > q) {return p;} return q;}
+
+u64 u64_min(u64 p,u64 q) {if(p < q) {return p;} return q;}
+u64 u64_max(u64 p,u64 q) {if(p > q) {return p;} return q;}
--- int.h
+++ int.h
@@ -14,5 +14,9 @@ typedef uint32_t  u32;
 typedef uint64_t  u64;
 
 i32 i32_min(i32 p,i32 q);
+i32 i32_max(i32 p,i32 q);
+
+u64 u64_min(u64 p,u64 q);
+u64 u64_max(u64 p,u64 q);
 
 #endif
